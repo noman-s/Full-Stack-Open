f7c9f17654056933845c3d8d0f44f9df0c46063f
noman <mnoman.sid4@gmail.com>
2024-12-02 09:18:39 -0500

    add ui to phonebook backend

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const base_url="http://localhost:3001/persons"
+const base_url = "/api/persons";
 
 const getAll = () => {
-  const request = axios.get(base_url)
-  return request.then(response => response.data)
-}
+  const request = axios.get(base_url);
+  return request.then((response) => response.data);
+};
 
-const create = newObject => {
-  const request = axios.post(base_url, newObject)
-  return request.then(response => response.data)
-}
+const create = (newObject) => {
+  const request = axios.post(base_url, newObject);
+  return request.then((response) => response.data);
+};
 
 const remove = (id) => {
-  const request = axios.delete(`${base_url}/${id}`)
-  return request.then(response => response.data)
-}
+  const request = axios.delete(`${base_url}/${id}`);
+  return request.then((response) => response.data);
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${base_url}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+  const request = axios.put(`${base_url}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 
-export default { getAll, create, remove, update}
+export default { getAll, create, remove, update };
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -97,6 +97,6 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`App listening on port ${PORT}`);
